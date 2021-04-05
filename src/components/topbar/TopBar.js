778a2684289bee1bff2f2886a8b5215fc1839819
Samuel Burke <sam.burke99@gmail.com>
2021-04-05 21:40:21 +0200

    final

--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -3,11 +3,11 @@ import React from 'react'
 const TopBar = () => {
     return (
         <div className='topbar'>
-            <h1>SNAP!</h1>
+            <h1 className='title'>SNAP!</h1>
             <div className='topbar__circle-container'>
                 <div className='topbar__circle'></div>
                 <div className='topbar__circle'></div>
-                <div className='topbar__circle'></div>
+                <div className='topbar__circle topbar__circle-3' ></div>
             </div>
         </div>
     )
